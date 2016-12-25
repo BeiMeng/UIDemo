599099b2b24038535f61a2918e14fd752cfb95be
BeiDream <331341164@qq.com>
2016-12-25 23:23:19 +0800

    添加项目文件。

---
+++ Bootstrap.jQuery/App_Start/Startup.Auth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+
+namespace Bootstrap.jQuery
+{
+    public partial class Startup
+    {
+        // 有关配置身份验证的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // 配置数据库上下文、用户管理器和登录管理器，以便为每个请求使用单个实例
+
+            // 取消注释以下行可允许使用第三方登录提供程序登录
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            //   appId: "",
+            //   appSecret: "");
+
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            //{
+            //    ClientId = "",
+            //    ClientSecret = ""
+            //});
+        }
+    }
+}
